86e10799f18489338b7db40d76a674fc0e4de220
Keith Isham <kisham8899@gmail.com>
2019-08-25 17:28:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ get '/newteam' do
 end 
 
 post '/team' do 
-  @name = params[:name]
-  @coach = params[:coach]
+  @name = params["name"]
+  @coach = params["coach"]
   
   @pg = params[:pg]
   @sg = params[:sg] 
